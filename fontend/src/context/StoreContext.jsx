87adaaf13b0b25d7af88a810fa183b7ea2d4f63d
tanvirtalukdersiam <149420877+tanvirtalukdersiam@users.noreply.github.com>
2025-01-20 19:28:49 +0600

    first commit

---
+++ fontend/src/context/StoreContext.jsx
@@ -0,0 +1,74 @@
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+import axios from "axios";
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:3000";
+  const [token, setToken] = useState("");
+  // const [food_list, setFoodList] = useState([]);
+  const addtoCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: (prev[itemId] || 0) + 1,
+    }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      const updatedCart = { ...prev };
+      if (updatedCart[itemId] > 1) {
+        updatedCart[itemId] -= 1;
+      } else {
+        delete updatedCart[itemId];
+      }
+      return updatedCart;
+    });
+  };
+
+  let getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  };
+
+  const fetchFoodList = async () => {
+    const response = await axios.get(url + "/api/food/list");
+    setFoodList(response.data.data);
+  };
+
+  useEffect(() => {
+    async function loadData() {
+      await fetchFoodList();
+      if (localStorage.getItem("token")) {
+        setToken(localStorage.getItem("token"));
+      }
+    }
+    loadData();
+  }, []);
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addtoCart,
+    removeFromCart,
+    getTotalCartAmount,
+    url,
+    token,
+    setToken,
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
